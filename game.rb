a538d4043cd84e3e881cf2ed75a8090b41cfdb48
duncanc19 <duncancarter93@gmail.com>
2020-09-23 14:26:03 +0100

    Add add_move method - slimed

--- game.rb
+++ game.rb
@@ -1,8 +1,12 @@
 class Game
 
   attr_accessor :game_state
-    
+
   def initialize
     @game_state = [['','', ''],['','', ''],['','', '']]
+  end
+
+  def add_move(row, col)
+    @game_state = [['x','', ''],['','', ''],['','', '']]
   end
 end
