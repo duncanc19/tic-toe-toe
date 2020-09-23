928d81df148f36f7f28679e8f841b9f19dea7442
Emma Blott <blott23@gmail.com>
2020-09-23 14:59:32 +0100

    wrote invalid move acceptance test - failing

--- spec/acceptance_tests_spec.rb
+++ spec/acceptance_tests_spec.rb
@@ -1,7 +1,7 @@
 # When: a new game starts
 # Then: display an empty grid
 describe 'a new game starts' do
-    xit 'displays an empty grid' do
+    it 'displays an empty grid' do
         # Arrange
         game = Game.new
         presenter = Present.new
@@ -13,13 +13,24 @@ describe 'a new game starts' do
     end
 end
 
-
-
-
 # Given: A grid
 # When: A player makes an invalid move
 # Then: the move is rejected
 # And: the player is asked for another move
+describe 'a player makes an invalid move' do
+    it 'rejects the move' do
+        # Arrange
+        game = Game.new
+
+        # Act
+        result = game.add_move(6,6)
+
+        # Assert
+        expect(result).to eq(false)
+    end
+end
+
+
 
 
 
