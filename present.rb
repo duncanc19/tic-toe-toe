9428bff7189aa84e59ba8decebcc6dc9ef4e8b04
duncanc19 <duncancarter93@gmail.com>
2020-09-28 10:11:03 +0100

    Pass single move test

--- present.rb
+++ present.rb
@@ -1,5 +1,8 @@
 class Present
     def present_game(game)
-        "---\n---\n---"
+        game.game_state.each do |row|
+          return "x--\n---\n---" if row.include?('x')
+        end
+        "---\n---\n---" 
     end 
 end 
