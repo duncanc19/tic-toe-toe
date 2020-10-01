b3c97f449a6885ea9bb73de805a11ce4d031573b
duncanc19 <duncancarter93@gmail.com>
2020-10-01 16:51:13 +0100

    Add enter move message to ask_user method

--- game_controller.rb
+++ game_controller.rb
@@ -5,4 +5,8 @@ class GameController
         @present = Present.new
         @present.output_to_command_line(@present.present_game(@game))
     end
+
+    def ask_user
+        print "Please enter your move: "
+    end
 end
