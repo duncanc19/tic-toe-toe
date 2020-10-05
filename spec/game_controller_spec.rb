5bd6921a79d9998653842bf559253afb7e78325e
duncanc19 <duncancarter93@gmail.com>
2020-10-05 10:37:26 +0100

    Add receive_move nil test in game_controller_spec - failing

--- spec/game_controller_spec.rb
+++ spec/game_controller_spec.rb
@@ -1,5 +1,14 @@
-# require_relative "../game_controller"
+require_relative "../game_controller"
 
-# context GameController do
-#     describe ""
-# end
+context GameController do
+    describe "#receive_move" do
+      it "returns nil when given empty string" do
+        # Arrange
+        game_controller = GameController.new
+        # Act
+        result = game_controller.receive_move 
+        # Assert  
+        expect(result).to eq(false)  
+      end
+    end
+end
