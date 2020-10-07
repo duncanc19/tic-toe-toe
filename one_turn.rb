6a6732c6922edcf149eb4a8bace6d30bc8967e14
duncanc19 <duncancarter93@gmail.com>
2020-10-07 14:12:21 +0100

    Add test for one turn invalid move loop - failing

--- one_turn.rb
+++ one_turn.rb
@@ -1,8 +1,12 @@
 class OneTurn 
     def play_turn(present)
         game_controller = present.game_controller
-        move = present.receive_input
-        game_controller.verify_and_format_move(move)
+        move = false
+        while !move do
+            move = present.receive_input
+            move = game_controller.verify_and_format_move(move)
+        end
+        
         game_controller.play_move 
         present.create_grid_as_string
         present.output_to_command_line
--- spec/one_turn_spec.rb
+++ spec/one_turn_spec.rb
@@ -3,7 +3,7 @@ require_relative '../one_turn'
 
 context OneTurn do
     describe "#play_turn" do
-        it "recieves on valid move and shows it on the board" do
+        it "receives on valid move and shows it on the board" do
             # Arrange 
             present = Present.new
             turn = OneTurn.new
@@ -15,7 +15,8 @@ context OneTurn do
             .to output(expected_string)
             .to_stdout_from_any_process
         end
-        it "recieves on invalid move, then a valid move so shows valid move on the board" do
+
+        it "receives an invalid(wrong format) move, then a valid move so shows valid move on the board" do
             # Arrange 
             present = Present.new
             turn = OneTurn.new
@@ -27,6 +28,20 @@ context OneTurn do
             expect { present.output_to_command_line }
             .to output(expected_string)
             .to_stdout_from_any_process
-            end
+        end
+
+        it "receives an invalid(game rules) move, then a valid move so shows valid move on the board" do
+            # Arrange 
+            present = Present.new
+            turn = OneTurn.new
+            allow(present.input).to receive(:gets).and_return("0,8", "0,2")
+            expected_string = "#{TEST_EMPTY}#{TEST_EMPTY}#{TEST_PLAYER1}\n#{TEST_THREE_EMPTY}\n#{TEST_THREE_EMPTY}\n"
+            # Act 
+            turn.play_turn(present)
+            # Assert (in {})
+            expect { present.output_to_command_line }
+            .to output(expected_string)
+            .to_stdout_from_any_process
+        end
     end
 end
