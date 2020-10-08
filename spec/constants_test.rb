e09dee92048608d35026e3994662b1e832feb147
Emma Blott <blott23@gmail.com>
2020-10-08 10:55:12 +0100

    add messages class - add welcome and std input message

--- spec/constants_test.rb
+++ spec/constants_test.rb
@@ -3,3 +3,4 @@ TEST_PLAYER2 = '🎃'
 TEST_EMPTY = '🔳'
 TEST_THREE_EMPTY = "#{TEST_EMPTY}#{TEST_EMPTY}#{TEST_EMPTY}"
 TEST_DEFAULT_INPUT_REQUEST = "Please enter your move: "
+TEST_WELCOME_MESSAGE = "Welcome to SCARY tic tac toe!! 🎃 vs 👻"
---
+++ spec/messages_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../messages'
+require_relative 'constants_test'
+
+context Messages do
+    describe "#message_hash" do
+        it "when given 'welcome' returns standard welcome message" do
+            # Act 
+            result = Messages.message_hash("welcome")
+            # Assert
+            expect(result).to eq(TEST_WELCOME_MESSAGE)
+        end
+        it "when given 'enter_move' returns standard enter message" do
+            # Act 
+            result = Messages.message_hash("enter_move")
+            # Assert
+            expect(result).to eq(TEST_DEFAULT_INPUT_REQUEST)
+        end
+    end
+end
