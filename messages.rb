f988882590cb0d366b6ae99dafd2f636a3a3533d
Emma Blott <blott23@gmail.com>
2020-10-08 10:57:53 +0100

    emoji refactor in CONSTANTS_TEST

--- messages.rb
+++ messages.rb
@@ -1,6 +1,6 @@
 class Messages
     def self.message_hash(message_name)
-        return "Welcome to SCARY tic tac toe!! 🎃 vs 👻" if message_name == 'welcome'
+        return "Welcome to SCARY tic tac toe!! 🎃 vs 👻 " if message_name == 'welcome'
         "Please enter your move: "
     end
 end
