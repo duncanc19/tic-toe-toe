8dcb5afe6fee93590368f72614ac8dc4f47f8625
duncanc19 <duncancarter93@gmail.com>
2020-10-12 16:18:32 +0100

    Extract game_controller as argument in one_turn

--- one_turn.rb
+++ one_turn.rb
@@ -1,22 +1,20 @@
 class OneTurn 
     def play_turn(present, game_controller)
-        get_valid_move(present)
+        get_valid_move(present, game_controller)
         present.create_grid_as_string
         present.output_to_command_line
     end
 
     
-    def get_valid_move(present)
-        game_controller = present.game_controller
+    def get_valid_move(present, game_controller)
         valid_move = false
         while !valid_move
-            valid_move = get_correct_format_input(present)
+            valid_move = get_correct_format_input(present, game_controller)
             valid_move =game_controller.play_move
         end 
     end
     
-    def get_correct_format_input(present)
-        game_controller = present.game_controller
+    def get_correct_format_input(present, game_controller)
         move = false
         while !move do
             move = present.receive_input
