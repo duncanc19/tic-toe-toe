730abb244aa6e57ea43bfce419b13c7b96aa1afc
Emma Blott <blott23@gmail.com>
2020-10-13 15:35:05 +0100

    change game_controller variable to game

--- one_turn.rb
+++ one_turn.rb
@@ -1,23 +1,23 @@
 class OneTurn 
-    def play_turn(present, get_input, game_controller)
-        get_valid_move(get_input, game_controller)
-        present.show_grid(game_controller)
+    def play_turn(present, get_input, game)
+        get_valid_move(get_input, game)
+        present.show_grid(game)
     end
 
     
-    def get_valid_move(get_input, game_controller)
+    def get_valid_move(get_input, game)
         valid_move = false
         while !valid_move
-            valid_move = get_correct_format_input(get_input, game_controller)
-            valid_move =game_controller.play_move
+            valid_move = get_correct_format_input(get_input, game)
+            valid_move =game.play_move
         end 
     end
     
-    def get_correct_format_input(get_input, game_controller)
+    def get_correct_format_input(get_input, game)
         move = false
         while !move do
             move = get_input.get_move
-            move = game_controller.verify_and_format_move(move)
+            move = game.verify_and_format_move(move)
         end
     end
 
