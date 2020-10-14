b825420fd20a58839c87baf548d5fbed25328016
duncanc19 <duncancarter93@gmail.com>
2020-10-14 10:04:10 +0100

    Delete one_turn files

--- main.rb
+++ main.rb
@@ -7,27 +7,10 @@ class Main
         # present.output_message_to_command_line_newline(Messages.message_hash(:welcome))
         present.show_grid(game)
         while (!CheckDraw.check_draw(game)) do
-
-            get_valid_move(get_input, game)
+            turn.get_valid_move(get_input, game)
             present.show_grid(game)
             break if CheckWin.check_win(game)
             game.next_player
-        end
-    end
-
-    def get_valid_move(get_input, game)
-        valid_move = false
-        while !valid_move
-            valid_move = get_correct_format_input(get_input, game)
-            valid_move = game.play_move
-        end 
-    end
-    
-    def get_correct_format_input(get_input, game)
-        move = false
-        while !move do
-            move = get_input.get_move
-            move = game.verify_and_format_move(move)
         end
     end
 end
