7efa4495138d9cd27982edeeb6a6ecdc46e36be2
duncanc19 <duncancarter93@gmail.com>
2020-10-15 09:23:43 +0100

    Remove unused methods from verify_input

--- verify_input.rb
+++ verify_input.rb
@@ -1,10 +1,5 @@
 class VerifyAndConvertInput 
 
-    # def take_and_return_user_input(move)
-    #     return false if !correct_format?(move) 
-    #     convert_move(move)
-    # end
-
     def verify_and_convert_move(move, game_state)
         return false if !correct_format?(move) 
         converted_move = convert_move(move)
@@ -33,22 +28,6 @@ class VerifyAndConvertInput
         col = move[1]
         return true if game_state[row][col] == 'o' || game_state[row][col] == 'x' 
         false
-    end
-
-    def get_valid_move(get_input, game)
-        valid_move = false
-        while !valid_move
-            valid_move = get_correct_format_input(get_input, game)
-            valid_move = game.play_move
-        end 
-    end
-    
-    def get_correct_format_input(get_input, game)
-        move = false
-        while !move do
-            move = get_input.get_move
-            move = game.verify_and_format_move(move)
-        end
     end
 
 end 
