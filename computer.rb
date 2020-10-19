21ff7aa33b150aed5147e15301b16e09fc1d39f0
duncanc19 <duncancarter93@gmail.com>
2020-10-19 15:36:54 +0100

    Add cloning of game_state array in winning_move to remove bug

--- computer.rb
+++ computer.rb
@@ -1,10 +1,12 @@
 class Computer
     def self.winning_move(game_state)
+        
         game_state.each_with_index do |row, row_index|
             row.each_with_index do |square, col_index| 
                 if square == ''
                     temp_game = Game.new
-                    temp_game.game_state = game_state
+                    temp_game.current_player = 'o'
+                    temp_game.game_state = game_state.map(&:clone)
                     temp_game.add_move(row_index, col_index)
                     return [row_index,col_index] if CheckWin.check_win(temp_game)
                 end
@@ -13,7 +15,9 @@ class Computer
         false
     end
     
-    def self.turn(game_state)
-        [1,1]
-    end
+    # def self.turn(game_state)
+    #     if winning_move(game_state)
+    #     [1,1]
+    # end
 end
+
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -19,27 +19,37 @@ context Computer do
                 # Assert
                 expect(move).to eq([0,0])
             end
-        end 
-    end
-    
-    describe ".turn" do
-        describe "two moves away from winnning" do
-            it "returns the best move[1,1]" do
+            it "returns false if no winning move" do
                 # Arrange
                 game_state = [['x','',''],['x','',''],['o','o','x']]
                 # Act
-                move = Computer.turn(game_state)
+                move = Computer.winning_move(game_state)
                 # Assert
-                expect(move).to eq([1,1])
+                expect(move).to eq(false)
             end
-            it "returns the best move[2,0]" do
-                # Arrange
-                game_state = [['','x',''],['o','x','x'],['','o','']]
-                # Act
-                move = Computer.turn(game_state)
-                # Assert
-                expect(move).to eq([2,0])
-            end
-        end
+        end 
     end
+
+    describe 
+    
+    # describe ".turn" do
+    #     describe "two moves away from winnning" do
+    #         it "returns the best move[1,1]" do
+    #             # Arrange
+    #             game_state = [['x','',''],['x','',''],['o','o','x']]
+    #             # Act
+    #             move = Computer.turn(game_state)
+    #             # Assert
+    #             expect(move).to eq([1,1])
+    #         end
+    #         it "returns the best move[2,0]" do
+    #             # Arrange
+    #             game_state = [['','x',''],['o','x','x'],['','o','']]
+    #             # Act
+    #             move = Computer.turn(game_state)
+    #             # Assert
+    #             expect(move).to eq([2,0])
+    #         end
+    #     end
+    # end
 end
