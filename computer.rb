9cf27b6b3377a8c3ab31ee2dfc6255de8c7dc180
Emma Blott <blott23@gmail.com>
2020-10-19 14:31:28 +0100

    add 2 moves to winning test and fixed Computer typo - failing

--- computer.rb
+++ computer.rb
@@ -1,7 +1,7 @@
 class Computer
     def self.turn(game_state)
         game_state.each_with_index do |row, row_index|
-            three_square.each_with_index do |square, col_index| 
+            row.each_with_index do |square, col_index| 
                 if square == ''
                     temp_game = Game.new
                     temp_game.game_state = game_state
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -2,21 +2,33 @@ require_relative '../computer'
 
 context Computer do
     describe ".turn" do
-        it "returns a winning move [0,2] given a winnable game" do
-            # Arrange
-            game_state = [['o','o',''],['x','x',''],['x','','']]
-            # Act
-            move = Computer.turn(game_state)
-            # Assert
-            expect(move).to eq([0,2])
-        end
-        it "returns a winning move [0,0] given a winnable game" do
-            # Arrange
-            game_state = [['','','x'],['x','o',''],['x','','o']]
-            # Act
-            move = Computer.turn(game_state)
-            # Assert
-            expect(move).to eq([0,0])
+        context "one move away from winning" do
+            it "returns a winning move [0,2] given a winnable game" do
+                # Arrange
+                game_state = [['o','o',''],['x','x',''],['x','','']]
+                # Act
+                move = Computer.turn(game_state)
+                # Assert
+                expect(move).to eq([0,2])
+            end
+            it "returns a winning move [0,0] given a winnable game" do
+                # Arrange
+                game_state = [['','','x'],['x','o',''],['x','','o']]
+                # Act
+                move = Computer.turn(game_state)
+                # Assert
+                expect(move).to eq([0,0])
+            end
+        end 
+        describe "two moves away from winnning" do
+            it "returns the best move[1,1]" do
+                # Arrange
+                game_state = [['x','',''],['x','',''],['o','o','x']]
+                # Act
+                move = Computer.turn(game_state)
+                # Assert
+                expect(move).to eq([1,1])
+            end
         end
     end
 end
