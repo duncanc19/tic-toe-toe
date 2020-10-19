046d7f67ad642e19b261de98144a074057066f48
Emma Blott <blott23@gmail.com>
2020-10-19 13:55:07 +0100

    add second winning move test - failing

--- computer.rb
+++ computer.rb
@@ -1,5 +1,5 @@
 class Computer
     def self.turn(game_state)
-
+        [0,2]
     end
 end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -2,13 +2,21 @@ require_relative '../computer'
 
 context Computer do
     describe ".turn" do
-        it "returns a winning move given a winnable game" do
+        it "returns a winning move [0,2] given a winnable game" do
             # Arrange
             game_state = [['o','o',''],['x','x',''],['x','','']]
             # Act
             move = Computer.turn(game_state)
             # Assert
             expect(move).to eq([0,2])
+        end
+        it "returns a winning move [0,0] given a winnable game" do
+            # Arrange
+            game_state = [['','','x'],['x','o',''],['x','','o']]
+            # Act
+            move = Computer.turn(game_state)
+            # Assert
+            expect(move).to eq([0,0])
         end
     end
 end
