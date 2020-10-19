c92ac397189abd5bb2be797ba567bb67b0e7f3c5
duncanc19 <duncancarter93@gmail.com>
2020-10-19 16:21:27 +0100

    Spiking minimum_move and maximum_move

--- computer.rb
+++ computer.rb
@@ -15,9 +15,42 @@ class Computer
         false
     end
     
-    # def self.turn(game_state)
-    #     if winning_move(game_state)
-    #     [1,1]
-    # end
+    def self.turn(game_state)
+        if !winning_move(game_state)
+            minimum_move(game_state)
+        end
+        [1,1]
+    end
+
+    def self.maximum_move(game_state)
+        game_state.each_with_index do |row, row_index|
+            row.each_with_index do |square, col_index| 
+                if square == ''
+                    temp_game = Game.new
+                    temp_game.current_player = 'o'
+                    temp_game.game_state = game_state.map(&:clone)
+                    temp_game.add_move(row_index, col_index)
+                    return [row_index,col_index] if CheckWin.check_win(temp_game)
+                    minimum_move(temp_game.game_state)
+                end
+            end
+        end
+    end
+
+    def self.minimum_move(game_state)
+        minimizer = []
+        game_state.each_with_index do |row, row_index|
+            row.each_with_index do |square, col_index| 
+                if square == ''
+                    temp_game = Game.new
+                    temp_game.current_player = 'x'
+                    temp_game.game_state = game_state.map(&:clone)
+                    temp_game.add_move(row_index, col_index)
+                    minimizer << -1 if CheckWin(temp_game)
+                    maximum_move()
+                end
+            end
+        end
+    end
 end
 
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -30,26 +30,32 @@ context Computer do
         end 
     end
 
-    describe 
-    
-    # describe ".turn" do
-    #     describe "two moves away from winnning" do
-    #         it "returns the best move[1,1]" do
-    #             # Arrange
-    #             game_state = [['x','',''],['x','',''],['o','o','x']]
-    #             # Act
-    #             move = Computer.turn(game_state)
-    #             # Assert
-    #             expect(move).to eq([1,1])
-    #         end
-    #         it "returns the best move[2,0]" do
-    #             # Arrange
-    #             game_state = [['','x',''],['o','x','x'],['','o','']]
-    #             # Act
-    #             move = Computer.turn(game_state)
-    #             # Assert
-    #             expect(move).to eq([2,0])
-    #         end
-    #     end
-    # end
+    describe ".maximum_move" do
+        
+    end
+
+    describe ".minimum_move" do
+
+    end
+
+    describe ".turn" do
+        describe "two moves away from winnning" do
+            xit "returns the best move[1,1]" do
+                # Arrange
+                game_state = [['x','',''],['x','',''],['o','o','x']]
+                # Act
+                move = Computer.turn(game_state)
+                # Assert
+                expect(move).to eq([1,1])
+            end
+            xit "returns the best move[2,0]" do
+                # Arrange
+                game_state = [['','x',''],['o','x','x'],['','o','']]
+                # Act
+                move = Computer.turn(game_state)
+                # Assert
+                expect(move).to eq([2,0])
+            end
+        end
+    end
 end
