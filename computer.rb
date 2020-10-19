03770bd9b34ae9b3a0be5e5293571f1ab01e67e0
Emma Blott <blott23@gmail.com>
2020-10-19 18:43:49 +0100

    messing with minimax - still ongoing

--- computer.rb
+++ computer.rb
@@ -22,7 +22,24 @@ class Computer
         [1,1]
     end
 
+    def self.top_layer(game_state)
+        
+        game_state.each_with_index do |row, row_index|
+            row.each_with_index do |square, col_index| 
+                if square == ''
+                    temp_game = Game.new
+                    temp_game.current_player = 'o'
+                    temp_game.game_state = game_state.map(&:clone)
+                    temp_game.add_move(row_index, col_index)
+                    return [row_index ,col_index]  if minimum_move(game_state) == 1 
+                end
+            end
+        end
+        false
+    end
+
     def self.maximum_move(game_state)
+        maximizer = [] 
         game_state.each_with_index do |row, row_index|
             row.each_with_index do |square, col_index| 
                 if square == ''
@@ -46,11 +63,15 @@ class Computer
                     temp_game.current_player = 'x'
                     temp_game.game_state = game_state.map(&:clone)
                     temp_game.add_move(row_index, col_index)
-                    minimizer << -1 if CheckWin(temp_game)
-                    maximum_move()
+                    if CheckWin(temp_game)
+                        minimizer << -1 
+                    else
+                        minimizer << maximum_move(temp_game.game_state)
+                    end
                 end
             end
         end
+        return minimizer.minimum()
     end
 end
 
