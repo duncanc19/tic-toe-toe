caa901040a2176f144ddadd5d4f474ae8683b4b6
duncanc19 <duncancarter93@gmail.com>
2020-10-19 14:12:41 +0100

    Add winning move check for turn in Computer

--- computer.rb
+++ computer.rb
@@ -1,5 +1,14 @@
 class Computer
     def self.turn(game_state)
-        [0,2]
+        game_state.each_with_index do |row, row_index|
+            three_square.each_with_index do |square, col_index| 
+                if square == ''
+                    temp_game = Game.new
+                    temp_game.game_state = game_state
+                    temp_game.add_move(row_index, col_index)
+                    return [row_index,col_index] if CheckWin.check_win(temp_game)
+                end
+            end
+        end
     end
 end
