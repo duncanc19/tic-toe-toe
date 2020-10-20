2dcf0c20cb907c8bf7f359a2909d5ccfcb6002fb
duncanc19 <duncancarter93@gmail.com>
2020-10-20 12:14:49 +0100

    Remove winning_move from Computer class

--- computer.rb
+++ computer.rb
@@ -2,22 +2,6 @@ require_relative "check_win"
 
 
 class Computer
-    def self.winning_move(game_state)
-        
-        game_state.each_with_index do |row, row_index|
-            row.each_with_index do |square, col_index| 
-                if square == ''
-                    temp_game = Game.new
-                    temp_game.current_player = 'o'
-                    temp_game.game_state = game_state.map(&:clone)
-                    temp_game.add_move(row_index, col_index)
-                    
-                end
-            end
-        end
-        false
-    end
-    
     def self.turn(game_state)
         top_layer(game_state)
     end
