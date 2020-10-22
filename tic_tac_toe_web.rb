ba72cb698acc5182307b20de587103974f7f0534
duncanc19 <duncancarter93@gmail.com>
2020-10-22 12:30:15 +0100

    Add grid_state storage in session

--- tic_tac_toe_web.rb
+++ tic_tac_toe_web.rb
@@ -1,5 +1,27 @@
 require 'sinatra'
 
-get '/' do
-  erb :index
-end
+  enable :sessions
+
+  get '/' do
+    @grid_array = fill_grid
+    session[:grid_array] = @grid_array
+    erb :index
+  end
+
+  post '/move' do
+    @grid_array = fill_grid(@params)
+    session[:grid_array] = @grid_array
+    erb :index
+  end
+
+
+  def fill_grid(params = [])
+    grid_array = [["","",""],["","",""],["","",""]]
+    params.each do |box, move|
+      row = box.match(/(\d)(\d)/)[1]
+      col = box.match(/(\d)(\d)/)[2]
+      grid_array[row.to_i][col.to_i] = move
+    end
+    return grid_array
+  end
+
